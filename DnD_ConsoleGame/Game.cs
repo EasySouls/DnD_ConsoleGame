2fe53f4e8bef8d472bd74bc39e09b67945ab82dc
tarjanyi.bence <tarjanyi.bence@NSZ-09>
2022-09-29 13:34:34 +0200

    Add project files.

---
+++ DnD_ConsoleGame/Game.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Game
+{
+    public Game() { }
+
+    private bool isPlaying = false;
+
+    // Functions
+    void initGame()
+    {
+
+    }
+
+    void mainMenu()
+    {
+
+    }
+
+    void createNewCharacter()
+    {
+
+    }
+
+    void levelUpCharacter()
+    { 
+
+    }
+
+    void saveCharacters()
+    {
+
+    }
+
+    void loadCharacters()
+    {
+
+    }
+
+    void selectCharacter()
+    {
+
+    }
+
+    void Travel()
+    {
+
+    }
+
+    void Rest()
+    {
+
+    }
+
+    // Accessors
+    public bool GetPlaying()
+    {
+        return isPlaying;
+    }
+}
+
---
+++ DnD_ConsoleGame/Program.cs
@@ -0,0 +1,8 @@
+﻿Game game;
+game.InitGame();
+
+while(game.GetPlaying())
+{
+    game.mainMenu();
+}
+
